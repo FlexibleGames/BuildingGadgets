48dd5caf8339fc5897fe00a71d709673edf4dfa1
Flexible Games <simtropbuggi@gmail.com>
2023-04-10 15:08:00 -0500

    Update to 1.18

--- code/BuildingGadget/Properties/AssemblyInfo.cs
+++ code/BuildingGadget/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Vintagestory.API.Common;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuildingGadget")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using Vintagestory.API.Common;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.18.0.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
